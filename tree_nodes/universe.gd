7479c7d11f5a97fbf8543003684a96e825d071b1
Jack Hill <jackhill2104@gmail.com>
2020-12-06 19:26:54 +1030

    Restore global singlton

--- tree_nodes/universe.gd
+++ tree_nodes/universe.gd
@@ -29,3 +29,25 @@
 extends Spatial
 
 const PERSIST_AS_PROCEDURAL_OBJECT := false
+
+const j_epoch_in_unix_time: float = 946690200.0 # Seconds between Jan 1st 1970 and Jan 1st 2000
+var j_epoch_dict := {
+	"year": 2000,
+	"month": 1,
+	"day": 1,
+	"hour": 0,
+	"minute": 0,
+	"second": 0
+}
+var start_time_dict := {
+	"year": 2020,
+	"month": 5,
+	"day": 10,
+	"hour": 0,
+	"minute": 0,
+	"second": 0
+}
+
+func _on_Universe_tree_entered():
+	var start_time_in_unix_time := OS.get_unix_time_from_datetime(start_time_dict)
+	Global.start_time = start_time_in_unix_time - OS.get_unix_time_from_datetime(j_epoch_dict)
